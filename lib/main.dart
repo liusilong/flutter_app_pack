be6f7ca90b8a474a48e4eedc1fcf2fb0d0c87ded
youfrweb <737064439@qq.com>
2021-09-11 15:38:58 +0800

    main-lsl first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo main-dev branch',
+      title: 'Flutter Demo main-lsl branch',
       theme: ThemeData(
         // This is the theme of your application.
         //
